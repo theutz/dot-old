17ce4d326c665cff92d61b26ec7055c4e1499694
Michael Utz <michael@theutz.dev>
2023-04-18 16:07:38 +0300

    Add lspsaga

--- programs/vim.nix
+++ programs/vim.nix
@@ -20,94 +20,130 @@
 
     globals = { mapleader = " "; };
 
-    maps = let
-      clearSearchWithEscape = {
-        action = "<cmd>noh<cr><esc>";
-        desc = "Escape and clear hlsearch";
+    maps =
+      let
+        clearSearchWithEscape = {
+          action = "<cmd>noh<cr><esc>";
+          desc = "Escape and clear hlsearch";
+        };
+      in
+      {
+        normal =
+          let
+            findFiles = {
+              action = "<cmd>Telescope find_files<cr>";
+              desc = "Find files";
+            };
+            searchInFiles = {
+              action = "<cmd>Telescope live_grep<cr>";
+              desc = "Search in files";
+            };
+            openTerminal = {
+              action = "<cmd>FloatermToggle<cr>";
+              desc = "Open terminal";
+            };
+          in
+          {
+            "<esc>" = clearSearchWithEscape;
+            "<leader>/" = searchInFiles;
+            "<leader><space>" = findFiles;
+            "<leader>bb" = {
+              action = "<cmd>Telescope buffers<cr>";
+              desc = "List buffers";
+            };
+            "<leader>ca" = {
+              action = "<cmd>Lspsaga code_action<cr>";
+              desc = "Code action";
+            };
+            "<leader>cd" = {
+              action = "<cmd>Lspsaga goto_definition<cr>";
+              desc = "Go to definition";
+            };
+            "<leader>cf" = {
+              action = "<cmd>Lspsaga lsp_finder<cr>";
+              desc = "Finder";
+            };
+            "<leader>ch" = {
+              action = "<cmd>Lspsaga hover_doc<cr>";
+              desc = "Do hover";
+            };
+            "<leader>cj" = {
+              action = "<cmd>Lspsaga diagnostic_jump_next<cr>";
+              desc = "Goto next error";
+            };
+            "<leader>ck" = {
+              action = "<cmd>Lspsaga diagnostic_jump_prev<cr>";
+              desc = "Goto prev error";
+            };
+            "<leader>cp" = {
+              action = "<cmd>Lspsaga preview_definition<cr>";
+              desc = "Peek definition";
+            };
+            "<leader>cr" = {
+              action = "<cmd>Lspsaga rename<cr>";
+              desc = "Rename";
+            };
+            "<leader>cx" = {
+              action = "<cmd>Lspsaga show_diagnostics<cr>";
+              desc = "Show diagnostics";
+            };
+            "<leader>fe" = {
+              action = "<cmd>Neotree<cr>";
+              desc = "Open file explorer";
+            };
+            "<leader>ff" = findFiles;
+            "<leader>fs" = {
+              action = "<cmd>w<cr>";
+              desc = "Save file";
+            };
+            "<leader>gg" = {
+              action =
+                "<cmd>FloatermNew --wintype=float --width=0.9 --height=0.9 lazygit<cr>";
+              desc = "Git Status";
+            };
+            "<leader>gm" = {
+              action = "<cmd>GitMessenger<cr>";
+              desc = "Git Messenger";
+            };
+            "<leader>ot" = openTerminal;
+            "<leader>qq" = {
+              action = "<cmd>xa<cr>";
+              desc = "Write and quit";
+            };
+            "<leader>qr" = {
+              action = "<cmd>source ~/.config/nvim/init.lua<cr>";
+              desc = "Reload neovim config";
+            };
+            "<leader>qs" = {
+              action = "<cmd>Obsession<cr>";
+              desc = "Toggle session tracking";
+            };
+            "<leader>qx" = {
+              action = "<cmd>Obsession!<cr>";
+              desc = "Delete session";
+            };
+            "<leader>sh" = {
+              action = "<cmd>Telescope help_tags<cr>";
+              desc = "Search help";
+            };
+            "<leader>sp" = searchInFiles;
+            "<leader>w" = {
+              action = "<c-w>";
+              desc = "window";
+            };
+            "<m-h>" = "<cmd>TmuxNavigateLeft<cr>";
+            "<m-j>" = "<cmd>TmuxNavigateDown<cr>";
+            "<m-k>" = "<cmd>TmuxNavigateUp<cr>";
+            "<m-l>" = "<cmd>TmuxNavigateRight<cr>";
+          };
+        insert = { "<esc>" = clearSearchWithEscape; };
+        terminal = {
+          "<esc><esc>" = {
+            action = "<c-\\><c-n>";
+            desc = "Enter normal mode";
+          };
+        };
       };
-    in {
-      normal = let
-        findFiles = {
-          action = "<cmd>Telescope find_files<cr>";
-          desc = "Find files";
-        };
-        searchInFiles = {
-          action = "<cmd>Telescope live_grep<cr>";
-          desc = "Search in files";
-        };
-        openTerminal = {
-          action = "<cmd>FloatermToggle<cr>";
-          desc = "Open terminal";
-        };
-      in {
-        "<esc>" = clearSearchWithEscape;
-        "<leader>/" = searchInFiles;
-        "<leader><space>" = findFiles;
-        "<leader>bb" = {
-          action = "<cmd>Telescope buffers<cr>";
-          desc = "List buffers";
-        };
-        "<leader>ca" = {
-          action = "<cmd>Lspsaga code_action<cr>";
-          desc = "Code action";
-        };
-        "<leader>fe" = {
-          action = "<cmd>Neotree<cr>";
-          desc = "Open file explorer";
-        };
-        "<leader>ff" = findFiles;
-        "<leader>fs" = {
-          action = "<cmd>w<cr>";
-          desc = "Save file";
-        };
-        "<leader>gg" = {
-          action =
-            "<cmd>FloatermNew --wintype=float --width=0.9 --height=0.9 lazygit<cr>";
-          desc = "Git Status";
-        };
-        "<leader>gm" = {
-          action = "<cmd>GitMessenger<cr>";
-          desc = "Git Messenger";
-        };
-        "<leader>ot" = openTerminal;
-        "<leader>qq" = {
-          action = "<cmd>xa<cr>";
-          desc = "Write and quit";
-        };
-        "<leader>qr" = {
-          action = "<cmd>source ~/.config/nvim/init.lua<cr>";
-          desc = "Reload neovim config";
-        };
-        "<leader>qs" = {
-          action = "<cmd>Obsession<cr>";
-          desc = "Toggle session tracking";
-        };
-        "<leader>qx" = {
-          action = "<cmd>Obsession!<cr>";
-          desc = "Delete session";
-        };
-        "<leader>sh" = {
-          action = "<cmd>Telescope help_tags<cr>";
-          desc = "Search help";
-        };
-        "<leader>sp" = searchInFiles;
-        "<leader>w" = {
-          action = "<c-w>";
-          desc = "window";
-        };
-        "<m-h>" = "<cmd>TmuxNavigateLeft<cr>";
-        "<m-j>" = "<cmd>TmuxNavigateDown<cr>";
-        "<m-k>" = "<cmd>TmuxNavigateUp<cr>";
-        "<m-l>" = "<cmd>TmuxNavigateRight<cr>";
-      };
-      insert = { "<esc>" = clearSearchWithEscape; };
-      terminal = {
-        "<esc><esc>" = {
-          action = "<c-\\><c-n>";
-          desc = "Enter normal mode";
-        };
-      };
-    };
 
     options = {
       number = true;
